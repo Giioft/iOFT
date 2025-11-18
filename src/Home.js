6c346856ca6fb6b49f07e88bd8d945037566c465
Giioft <softgideon1@gmail.com>
2025-11-18 09:35:33 +0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Sidebar from './Sidebar';
 
 function Home() {
   return (
     <div>
       <Navbar />
-      <h1>Welcome to iOFT OS</h1>
-      <p>Your AI Operating System.</p>
+      <div style={{ display: 'flex' }}>
+        <Sidebar />
+
+        <div style={{ padding: '20px' }}>
+          <h1>Welcome to iOFT OS</h1>
+          <p>Your AI Operating System.</p>
+        </div>
+
+      </div>
     </div>
   );
 }
