2c119d2787e945bd8695fc2c38cf08fb232ebfef
Giioft <softgideon1@gmail.com>
2025-11-18 08:10:17 +0300

    Create App.js

---
+++ app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Welcome to iOFT</h1>
+      <p>Your AI OS is now running.</p>
+    </div>
+  );
+}
+
+export default App;
